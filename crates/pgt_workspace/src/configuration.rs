06f680b3f2e248419f8134f50358e612ea8e8218
psteinroe <philipp@steinroetter.com>
2025-04-15 08:46:29 +0200

    fix: always use latest schema

--- crates/pgt_workspace/src/configuration.rs
+++ crates/pgt_workspace/src/configuration.rs
@@ -170,14 +170,12 @@ pub fn create_config(
     })?;
 
     // we now check if postgrestools is installed inside `node_modules` and if so, we use the schema from there
-    if VERSION == "0.0.0" {
-        let schema_path = Path::new("./node_modules/@postgrestools/postgrestools/schema.json");
-        let options = OpenOptions::default().read(true);
-        if fs.open_with_options(schema_path, options).is_ok() {
-            configuration.schema = schema_path.to_str().map(String::from);
-        }
+    let schema_path = Path::new("./node_modules/@postgrestools/postgrestools/schema.json");
+    let options = OpenOptions::default().read(true);
+    if fs.open_with_options(schema_path, options).is_ok() {
+        configuration.schema = schema_path.to_str().map(String::from);
     } else {
-        configuration.schema = Some(format!("https://pgtools.dev/schemas/{VERSION}/schema.json"));
+        configuration.schema = Some("https://pgtools.dev/schemas/latest/schema.json".to_string());
     }
 
     let contents = serde_json::to_string_pretty(&configuration)
