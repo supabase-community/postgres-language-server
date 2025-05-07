26e82c7ef9572c6ec912a17f1b949b5b5dc30a78
Will Ruggiano <11872440+willruggiano@users.noreply.github.com>
2025-05-07 01:18:22 -0700

    feat: allow configuration through workspace/didChangeConfiguration (#316)
    ## What kind of change does this PR introduce?
    
    It enables passing workspace settings via the
    workspace/didChangeConfiguration notification. This in turn enables the
    client to specify settings dynamically, rather than being limited to
    configuration files. This is _a_ solution to #302. See example usage
    (with lspconfig) here:
    https://github.com/willruggiano/neovim.drv/commit/9aa06ad7c889ed7b14d35ab34264e757d5ad1a7e.
    
    ## What is the current behavior?
    
    There is none. The payload of this handler is currently ignored.
    
    ## What is the new behavior?
    
    The configuration received by the handler is merged with the fs
    configuration.
    
    ---------
    
    Co-authored-by: Will Ruggiano <wmruggiano@gmail.com>

--- crates/pgt_lsp/src/session.rs
+++ crates/pgt_lsp/src/session.rs
@@ -3,10 +3,11 @@ use crate::diagnostics::LspError;
 use crate::documents::Document;
 use crate::utils;
 use anyhow::Result;
+use biome_deserialize::Merge;
 use futures::StreamExt;
 use futures::stream::FuturesUnordered;
 use pgt_analyse::RuleCategoriesBuilder;
-use pgt_configuration::ConfigurationPathHint;
+use pgt_configuration::{ConfigurationPathHint, PartialConfiguration};
 use pgt_diagnostics::{DiagnosticExt, Error};
 use pgt_fs::{FileSystem, PgTPath};
 use pgt_workspace::Workspace;
@@ -386,11 +387,13 @@ impl Session {
     /// This function attempts to read the `postgrestools.jsonc` configuration file from
     /// the root URI and update the workspace settings accordingly
     #[tracing::instrument(level = "trace", skip(self))]
-    pub(crate) async fn load_workspace_settings(&self) {
+    pub(crate) async fn load_workspace_settings(&self, extra_config: Option<PartialConfiguration>) {
         // Providing a custom configuration path will not allow to support workspaces
         if let Some(config_path) = &self.config_path {
             let base_path = ConfigurationPathHint::FromUser(config_path.clone());
-            let status = self.load_pgt_configuration_file(base_path).await;
+            let status = self
+                .load_pgt_configuration_file(base_path, extra_config)
+                .await;
             self.set_configuration_status(status);
         } else if let Some(folders) = self.get_workspace_folders() {
             info!("Detected workspace folder.");
@@ -401,9 +404,10 @@ impl Session {
                 match base_path {
                     Ok(base_path) => {
                         let status = self
-                            .load_pgt_configuration_file(ConfigurationPathHint::FromWorkspace(
-                                base_path,
-                            ))
+                            .load_pgt_configuration_file(
+                                ConfigurationPathHint::FromWorkspace(base_path),
+                                extra_config.clone(),
+                            )
                             .await;
                         self.set_configuration_status(status);
                     }
@@ -420,7 +424,9 @@ impl Session {
                 None => ConfigurationPathHint::default(),
                 Some(path) => ConfigurationPathHint::FromLsp(path),
             };
-            let status = self.load_pgt_configuration_file(base_path).await;
+            let status = self
+                .load_pgt_configuration_file(base_path, extra_config)
+                .await;
             self.set_configuration_status(status);
         }
     }
@@ -428,16 +434,21 @@ impl Session {
     async fn load_pgt_configuration_file(
         &self,
         base_path: ConfigurationPathHint,
+        extra_config: Option<PartialConfiguration>,
     ) -> ConfigurationStatus {
         match load_configuration(&self.fs, base_path.clone()) {
             Ok(loaded_configuration) => {
                 let LoadedConfiguration {
-                    configuration: fs_configuration,
+                    configuration: mut fs_configuration,
                     directory_path: configuration_path,
                     ..
                 } = loaded_configuration;
                 info!("Configuration loaded successfully from disk.");
                 info!("Update workspace settings.");
+
+                if let Some(ws_configuration) = extra_config {
+                    fs_configuration.merge_with(ws_configuration);
+                }
 
                 let result = fs_configuration
                     .retrieve_gitignore_matches(&self.fs, configuration_path.as_deref());
