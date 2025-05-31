d23a67827a96c13ace065f6677f63a39effbe865
Julian <juliandomke@outlook.de>
2025-05-31 16:18:20 +0200

    ok

--- crates/pgt_test_utils/testdb_migrations/0001_setup-roles.sql
+++ crates/pgt_test_utils/testdb_migrations/0001_setup-roles.sql
@@ -1,23 +1,23 @@
 do $$
 begin
 
--- begin
---   create role owner superuser createdb login bypassrls;
--- exception when duplicate_object then
---   null;
--- end;
+begin
+  create role owner superuser createdb login bypassrls;
+exception when duplicate_object then
+  null;
+end;
 
--- begin
---   create role test_login login;
--- exception when duplicate_object then
---   null;
--- end;
+begin
+  create role test_login login;
+exception when duplicate_object then
+  null;
+end;
 
--- begin
---   create role test_nologin;
--- exception when duplicate_object then
---   null;
--- end;
+begin
+  create role test_nologin;
+exception when duplicate_object then
+  null;
+end;
 
 end 
 $$;
