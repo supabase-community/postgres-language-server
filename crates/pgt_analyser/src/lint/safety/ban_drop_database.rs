7a9a1e1c835706f8989fedf3496d7b7917313be5
Julian <juliandomke@outlook.de>
2025-06-19 10:13:39 +0200

    fixed

--- crates/pgt_analyser/src/lint/safety/ban_drop_database.rs
+++ crates/pgt_analyser/src/lint/safety/ban_drop_database.rs
@@ -21,19 +21,17 @@ impl Rule for BanDropDatabase {
     fn run(ctx: &RuleContext<Self>) -> Vec<RuleDiagnostic> {
         let mut diagnostics = vec![];
 
-        if let pgt_query_ext::NodeEnum::DropStmt(stmt) = &ctx.stmt() {
-            if stmt.remove_type() == pgt_query_ext::protobuf::ObjectType::ObjectDatabase {
-                diagnostics.push(
-                    RuleDiagnostic::new(
-                        rule_category!(),
-                        None,
-                        markup! {
-                            "Dropping a database may break existing clients."
-                        },
-                    )
-                    .detail(None, "You probably don't want to drop your database."),
-                );
-            }
+        if let pgt_query_ext::NodeEnum::DropdbStmt(stmt) = &ctx.stmt() {
+            diagnostics.push(
+                RuleDiagnostic::new(
+                    rule_category!(),
+                    None,
+                    markup! {
+                        "Dropping a database may break existing clients."
+                    },
+                )
+                .detail(None, "You probably don't want to drop your database."),
+            );
         }
 
         diagnostics
