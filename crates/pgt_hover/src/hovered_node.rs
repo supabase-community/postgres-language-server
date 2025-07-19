97f42fcfa34a02edec57506465a9dcfa0d749ed4
Julian <juliandomke@outlook.de>
2025-07-19 17:10:09 +0200

    ok?

--- crates/pgt_hover/src/hovered_node.rs
+++ crates/pgt_hover/src/hovered_node.rs
@@ -1,6 +1,5 @@
 use pgt_text_size::TextSize;
-
-use crate::OnHoverParams;
+use pgt_treesitter::TreeSitterContextParams;
 
 pub(crate) enum NodeIdentification {
     Name(String),
@@ -19,5 +18,28 @@ pub(crate) enum HoveredNode {
 }
 
 impl HoveredNode {
-    pub(crate) fn get(position: TextSize, cst: &tree_sitter::Tree) -> Self {}
+    pub(crate) fn get(position: TextSize, text: &str, tree: &tree_sitter::Tree) -> Option<Self> {
+        let ctx = pgt_treesitter::context::TreesitterContext::new(TreeSitterContextParams {
+            position,
+            text,
+            tree,
+        });
+
+        let node_content = ctx.get_node_under_cursor_content()?;
+        let under_node = ctx.node_under_cursor?;
+
+        match under_node.kind() {
+            "relation" => {
+                if let Some(schema) = ctx.schema_or_alias_name {
+                    Some(HoveredNode::Table(NodeIdentification::SchemaAndName((
+                        schema,
+                        node_content,
+                    ))))
+                } else {
+                    Some(HoveredNode::Table(NodeIdentification::Name(node_content)))
+                }
+            }
+            _ => None,
+        }
+    }
 }
--- crates/pgt_hover/src/lib.rs
+++ crates/pgt_hover/src/lib.rs
@@ -1,19 +1,50 @@
 use pgt_schema_cache::SchemaCache;
 use pgt_text_size::TextSize;
 
+use crate::{hovered_node::HoveredNode, to_markdown::ToHoverMarkdown};
+
 mod hovered_node;
 mod to_markdown;
 
 pub struct OnHoverParams<'a> {
     pub position: TextSize,
     pub schema_cache: &'a SchemaCache,
+    pub stmt_sql: &'a str,
     pub ast: Option<&'a pgt_query_ext::NodeEnum>,
     pub ts_tree: &'a tree_sitter::Tree,
 }
 
-pub fn on_hover(_params: OnHoverParams) -> Vec<String> {
+pub fn on_hover(params: OnHoverParams) -> Vec<String> {
     // needs to find the right element(s) in the schema_cache
     // then, we should map the schema_cache items into markdown strings.
 
-    vec![]
+    if let Some(hovered_node) = HoveredNode::get(params.position, params.stmt_sql, params.ts_tree) {
+        match hovered_node {
+            HoveredNode::Table(node_identification) => {
+                let table = match node_identification {
+                    hovered_node::NodeIdentification::Name(n) => {
+                        params.schema_cache.find_table(n.as_str(), None)
+                    }
+                    hovered_node::NodeIdentification::SchemaAndName((s, n)) => {
+                        params.schema_cache.find_table(n.as_str(), Some(s.as_str()))
+                    }
+                    hovered_node::NodeIdentification::SchemaAndTableAndName(_) => None,
+                };
+
+                table
+                    .map(|t| {
+                        let mut markdown = String::new();
+                        match t.to_hover_markdown(&mut markdown) {
+                            Ok(_) => vec![markdown],
+                            Err(_) => vec![],
+                        }
+                    })
+                    .unwrap_or(vec![])
+            }
+
+            _ => todo!(),
+        }
+    } else {
+        Default::default()
+    }
 }
