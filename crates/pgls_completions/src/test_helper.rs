6bda5237bcd23ca9bec4843c3f6e6181e0d3f380
Julian <juliandomke@outlook.de>
2026-01-23 18:15:31 +0100

    ack

--- crates/pgls_completions/src/test_helper.rs
+++ crates/pgls_completions/src/test_helper.rs
@@ -206,8 +206,6 @@ pub(crate) async fn assert_no_complete_results(query: &str, setup: Option<&str>,
     let params = get_test_params(&tree, &cache, query.into());
     let items = complete(params);
 
-    println!("Items returned: {:#?}", items);
-
     assert_eq!(items.len(), 0)
 }
 
